752e2b6cdb96693c606c42f8e5bc8e829b44c0d8
Mauricio Rojas <mrojasb2000@gmail.com>
2019-05-18 19:25:48 -0400

    Refactor heap structures.

--- chapter01/heaps.go
+++ chapter01/heaps.go
@@ -29,7 +29,8 @@ func (iheap *IntegerHeap) Push(heapintf interface{}) {
 func (iheap *IntegerHeap) Pop() interface{} {
 	var n int
 	var x1 int
-	var previous IntegerHeap = *iheap
+	//var previous IntegerHeap = *iheap
+	var previous = *iheap
 	n = len(previous)
 	x1 = previous[n-1]
 	*iheap = previous[0 : n-1]
@@ -38,7 +39,8 @@ func (iheap *IntegerHeap) Pop() interface{} {
 
 // main method
 func main() {
-	var intHeap *IntegerHeap = &IntegerHeap{1, 4, 5}
+	//var intHeap *IntegerHeap = &IntegerHeap{1, 4, 5}
+	var intHeap = &IntegerHeap{1, 4, 5}
 
 	heap.Init(intHeap)
 	heap.Push(intHeap, 2)
