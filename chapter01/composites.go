319fb2a027d5deeb9e95ff49631bd83ea4b231f2
Mauricio Rojas <mrojasb2000@gmail.com>
2019-05-19 14:52:11 -0400

    Add new leaf to new branch.

--- chapter01/composites.go
+++ chapter01/composites.go
@@ -61,9 +61,11 @@ func main() {
 	var leaf1 = Leaflet{name: "leaf 1"}
 	var leaf2 = Leaflet{name: "leaf 2"}
 	var branch2 = Branch{name: "branch 2"}
+	var leaf3 = Leaflet{name: "leaf 3"}
 
 	branch.add(leaf1)
 	branch.add(leaf2)
+	branch2.add(leaf3)
 	branch.addBranch(branch2)
 	branch.perfom()
 }
